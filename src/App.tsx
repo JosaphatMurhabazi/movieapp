4ac609692a170df71362c7df742153ae8490d16f
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-11-26 13:10:38 +0100

    Add query parameter to fetchMovies

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,13 @@ const App = () => {
     const [movieList, setMovieList] = useState<Movie[]>([]);
     const [isLoading, setIsLoading] = useState(false)
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query='') => {
         setIsLoading(true);
         setErrorMessage('');
 
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint,API_OPTIONS);
             if(!response.ok) throw new Error('Failed to fetch movies');
 
@@ -55,8 +56,8 @@ const App = () => {
     }
 
     useEffect(() => {
-        fetchMovies();
-    }, []);
+        fetchMovies(searchTerm);
+    }, [searchTerm]);
 
     return (
         <main>
