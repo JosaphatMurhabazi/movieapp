368c0d363b385cb3d601e6efa5286c3229e934b1
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-11-26 12:13:22 +0100

    Display movies

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import {useState, useEffect} from "react";
 import './App.css'
 import Search from "./components/Search.tsx";
 
+interface Movie {
+    id: number;
+    title: string;
+    vote_average: number;
+    poster_path: string | null;
+    release_date: string;
+    original_language: string;
+
+}
+
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
@@ -15,12 +25,13 @@ const API_OPTIONS = {
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
-    const [movieList, setMovieList] = useState([]);
+    const [movieList, setMovieList] = useState<Movie[]>([]);
     const [isLoading, setIsLoading] = useState(false)
 
     const fetchMovies = async () => {
         setIsLoading(true);
         setErrorMessage('');
+
         try {
             const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint,API_OPTIONS);
@@ -53,16 +64,25 @@ const App = () => {
                 <header>
                     <img src="./hero.png" alt="Hero Banner"/>
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
 
                 <section className="all-movies">
                     <h2>All Movies</h2>
-
-                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
+                    {isLoading ?(
+                        <p className="white">Loading...</p>
+                    ):errorMessage ?(
+                        <p className="text-red-500">{errorMessage}</p>
+                    ):(
+                        <ul>
+                            {movieList.map((movie) => (
+                                <p className="text-white" key={movie.id}>{movie.title}</p>
+                            ))}
+                        </ul>
+                    )}
                 </section>
 
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             </div>
         </main>
     )
